69da74d754ea5ef3742de5bc9b75d7e1d88a535c
rjvb7424 <rjvb7424@leeds.ac.uk>
2026-04-28 18:12:25 +0100

    clean commit

---
+++ frontend/.vite-cache/deps/@mui_icons-material_Speed.js
@@ -0,0 +1,8 @@
+"use client";
+import { t as require_jsx_runtime } from "./react_jsx-runtime.js";
+import { t as createSvgIcon } from "./createSvgIcon-Dyouk0YR.js";
+var Speed_default = createSvgIcon(/* @__PURE__ */ (0, require_jsx_runtime().jsx)("path", { d: "m20.38 8.57-1.23 1.85a8 8 0 0 1-.22 7.58H5.07A8 8 0 0 1 15.58 6.85l1.85-1.23A10 10 0 0 0 3.35 19a2 2 0 0 0 1.72 1h13.85a2 2 0 0 0 1.74-1 10 10 0 0 0-.27-10.44zm-9.79 6.84a2 2 0 0 0 2.83 0l5.66-8.49-8.49 5.66a2 2 0 0 0 0 2.83" }), "Speed");
+//#endregion
+export { Speed_default as default };
+
+//# sourceMappingURL=@mui_icons-material_Speed.js.map
